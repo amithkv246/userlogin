7b4ae66670051043728d229b3cdcb08b50f6b1c3
amithkv246 <amithkv246@gmail.com>
2024-11-12 12:18:44 +0530

    Added conditional rendering for email validation.

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -5,24 +5,37 @@ import Button from "../../components/button";
 export default function LoginPage() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [isEmailValid, setisEmailValid] = useState(true)
 
     function handlelogin() {
         alert(`Email: ${email}, Password: ${password}`)
     }
-
+    // alert(email.length)
     useEffect(() => {
         console.log("useEffect triggered")
-        const emailregexp=/^[a-z][a-zA-Z0-9-_.]+@[a-zA-Z]+\.[a-zA-Z]{2,4}$/ 
-        if(emailregexp.test(email)) {
-            console.log("Valid email")
+        const emailregexp=/^[a-z][a-zA-Z0-9-_.]{0,}@[a-zA-Z]+\.[a-zA-Z]{2,4}$/
+        if (!emailregexp.test(email) && email.length !== 0) {
+            setisEmailValid(false)
         }else {
-            console.log("Invalid email")
+            setisEmailValid(true)
         }
+        const passregexp=/^ [a-zA-Z0-9]{8}
 
-        if (password.length<=7) {
-            console.log("The password is too short")
-        }
     },[email, password])
+
+    // useEffect(() => {
+    //     console.log("useEffect triggered")
+    //     const emailregexp=/^[a-z][a-zA-Z0-9-_.]+@[a-zA-Z]+\.[a-zA-Z]{2,4}$/ 
+    //     if(emailregexp.test(email)) {
+    //         console.log("Valid email")
+    //     }else {
+    //         console.log("Invalid email")
+    //     }
+
+    //     if (password.length<=7) {
+    //         console.log("The password is too short")
+    //     }
+    // },[email, password])
 
     return (
         <React.Fragment>
@@ -30,6 +43,10 @@ export default function LoginPage() {
                 <div className="bg-light w-25 p-3 text-center rounded-3" style={{ boxShadow: "0px 0px 10px 5px #aaa" }}>
                     <p className="fs-3 fw-semibold text-primary">Login</p>
                     <TextInput type={"email"} placeholder={"example@gmail.com"} onChange={(e) => setEmail(e.target.value)} />
+                    {
+                        !isEmailValid &&
+                        <p className="text-danger" style={{fontSize: "small"}}>Email is invalid</p>
+                    }
                     <TextInput type={"password"} placeholder={"********"} onChange={(e) => setPassword(e.target.value)}/>
                     <Button label={"Login"} onClick={handlelogin}/>
                     <p style={{ fontSize: "small" }}>Don't have an account? <span className="text-primary">Create an account</span></p>
